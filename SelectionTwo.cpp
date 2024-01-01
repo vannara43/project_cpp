44033bc8badb36d4878cb0c643b3266b492eda28
Vannara43 <vannarathong@gmail.com>
2024-01-01 08:38:35 -0800

    updated gold farm

--- SelectionTwo.cpp
+++ SelectionTwo.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <iomanip>
+#include <cstdlib>
 
 using namespace std;
 
-static int SelectionTwo() {
+static int SelectionTwo(int gold) {
+
+	unsigned seed = time(0);
+	srand(seed);
+	int findGold = (rand() % 100 + 1);
+
 	cout << "You chosen to farm gold." << endl;
 
-	return 0;
+	for (int i = 0; i < findGold; i++) {
+		int printThis = i+1;
+		for (int j = 0; j < printThis; j++) {
+		cout << "$";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	cout << "You found " << findGold << " gold!" << endl << endl;
+
+	return findGold;
 }
