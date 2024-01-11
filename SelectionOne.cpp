85fa71b266f697cbcc7e0722f70dc579620ca5a7
Vannara43 <vannarathong@gmail.com>
2024-01-11 03:48:23 -0800

    updated text for shop items

--- SelectionOne.cpp
+++ SelectionOne.cpp
@@ -6,11 +6,11 @@ using namespace std;
 static int SelectionOne() {
 	cout << "Hello, welcome to the shop." << endl;
 	cout << "Make a selection(1-5):" << endl;
-	cout << "1. Apple" << endl;
-	cout << "2. Banana" << endl;
-	cout << "3. Corn" << endl;
-	cout << "4. Kiwi" << endl;
-	cout << "5. Mango" << endl;
+	cout << "1. Apple(5 Gold)" << endl;
+	cout << "2. Banana(10 Gold)" << endl;
+	cout << "3. Corn(20 Gold)" << endl;
+	cout << "4. Kiwi(50 Gold)" << endl;
+	cout << "5. Mango(100 Gold)" << endl;
 
 
 	return 0;
